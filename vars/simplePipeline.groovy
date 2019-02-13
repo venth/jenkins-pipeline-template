abad68ced4cb8d1eddc0120d04f57bc219cfe853
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2019-02-13 09:54:50 +0100

    Create simplePipeline.groovy

---
+++ vars/simplePipeline.groovy
@@ -0,0 +1,12 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        // our complete declarative pipeline can go in here
+        ...
+    }
+}
