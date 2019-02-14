b4ea577006e8d77c3ea7bc5aa0af45a0cd53fadb
Artur Krysiak <artur.krysiak@hrs.com>
2019-02-14 13:31:27 +0100

    added container definition

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -5,52 +5,65 @@ def call(body) {
     body.delegate = pipelineParams
     body()
 
-    pipeline {
-        agent any
-        stages {
-            stage('checkout git') {
-                steps {
-                    git branch: pipelineParams.branch, credentialsId: pipelineParams.credentialsId, url: pipelineParams.scmUrl
-                }
-            }
 
-            stage('build') {
-                steps {
-                    sh './gradlew clean build'
-                }
-            }
+    def label = "mypod-${UUID.randomUUID().toString()}"
+    podTemplate(label: label, containers: [
+        containerTemplate(
+                name: 'java',
+                image: 'openjdk:11-jre',
+                ttyEnabled: true, command: 'cat',
+                volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')])]) {
 
-            stage ('test') {
-                steps {
-                    parallel (
-                        "unit tests": { echo "sh './gradlew test'" },
-                        "integration tests": { echo "sh 'mvn integration-test'" }
-                    )
-                }
-            }
+            pipeline {
+                stages {
+                    stage('checkout git') {
+                        steps {
+                            git branch: pipelineParams.branch, credentialsId: pipelineParams.credentialsId, url: pipelineParams.scmUrl
+                        }
+                    }
 
-            stage('deploy developmentServer'){
-                steps {
-                    deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
-                }
-            }
+                    node(label) {
+                        container('java') {
+                            stage('build') {
+                                steps {
+                                    sh './gradlew clean build'
+                                }
+                            }
+                        }
+                    }
 
-            stage('deploy staging'){
-                steps {
-                    deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
-                }
-            }
+                    stage ('test') {
+                        steps {
+                            parallel (
+                                    "unit tests": { echo "sh './gradlew test'" },
+                                    "integration tests": { echo "sh 'mvn integration-test'" }
+                            )
+                        }
+                    }
 
-            stage('deploy production'){
-                steps {
-                    deploy(pipelineParams.productionServer, pipelineParams.serverPort)
+                    stage('deploy developmentServer'){
+                        steps {
+                            deploy(pipelineParams.developmentServer, pipelineParams.serverPort)
+                        }
+                    }
+
+                    stage('deploy staging'){
+                        steps {
+                            deploy(pipelineParams.stagingServer, pipelineParams.serverPort)
+                        }
+                    }
+
+                    stage('deploy production'){
+                        steps {
+                            deploy(pipelineParams.productionServer, pipelineParams.serverPort)
+                        }
+                    }
+                }
+                post {
+                    failure {
+                        mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
+                    }
                 }
             }
         }
-        post {
-            failure {
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-            }
-        }
-    }
 }
